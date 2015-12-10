d6e0ad96fb139cfaa362bef8a463505ec3e07112
Natalie Wolfe <natalie@lifewanted.com>
2015-12-10 15:37:55 -0800

    Fixed pipe tests on OS X.

--- tests/io/PipeTests.cpp
+++ tests/io/PipeTests.cpp
@@ -7,6 +7,7 @@
 #include "lw/event.hpp"
 #include "lw/io.hpp"
 #include "lw/memory.hpp"
+#include "lw/pp.hpp"
 
 using namespace std::chrono_literals;
 
@@ -15,8 +16,9 @@ namespace tests {
 
 struct PipeTests : public testing::Test {
     event::Loop loop;
-    int pipes[2];
-    std::string content_str = "an awesome message to keep";
+    int pipe_descriptors[2];
+    const std::string content_str   = "an awesome message to keep";
+    const std::string pipe_name     = "liblw_pipe_tests_named_pipe";
     memory::Buffer contents;
 
     PipeTests(void):
@@ -26,12 +28,12 @@ struct PipeTests : public testing::Test {
     }
 
     void SetUp(void) override {
-        ::pipe(pipes);
+        ::pipe(pipe_descriptors);
     }
 
     void TearDown(void) override {
-        ::close(pipes[0]);
-        ::close(pipes[1]);
+        ::close(pipe_descriptors[0]);
+        ::close(pipe_descriptors[1]);
     }
 };
 
@@ -40,32 +42,51 @@ struct PipeTests : public testing::Test {
 TEST_F(PipeTests, Read){
     io::Pipe pipe(loop);
     bool started = false;
+    bool buffer_received = false;
     bool finished = false;
     bool promise_called = false;
 
     // Set up the pipe to read.
-    pipe.open(pipes[0]);
+    pipe.open(pipe_descriptors[0]);
     pipe.read([&](const std::shared_ptr<const memory::Buffer>& buffer){
+        LW_TRACE("Read callback.");
+        SCOPED_TRACE("Read callback.");
+
+        buffer_received = true;
         EXPECT_TRUE(started);
         EXPECT_FALSE(finished);
         EXPECT_EQ(contents, *buffer);
     }).then([&](const std::size_t bytes_read){
+        LW_TRACE("Read completion callback.");
+        SCOPED_TRACE("Read completion callback.");
+
         promise_called = true;
         EXPECT_TRUE(started);
+        EXPECT_TRUE(buffer_received);
         EXPECT_FALSE(finished);
+        EXPECT_LT( 0, bytes_read );
         EXPECT_EQ(contents.size(), bytes_read);
     });
 
     // Separately, create a timeout that will send the data on the pipe once the loop has started.
     event::wait(loop, 0s).then([&](){
-        ::write(pipes[1], content_str.c_str(), content_str.size());
-        ::fsync(pipes[1]);
-        ::close(pipes[1]);
+        LW_TRACE("Delayed write of " << content_str.size() << " bytes: \"" << content_str << "\".");
+        SCOPED_TRACE("Delayed write.");
+        ::write(pipe_descriptors[1], content_str.c_str(), content_str.size());
+        ::fsync(pipe_descriptors[1]);
+
+        return event::wait(loop, 500ms).then([&](){
+            LW_TRACE("Delayed closing of pipe.");
+            SCOPED_TRACE("Delayed closing of pipe.");
+            ::close(pipe_descriptors[1]);
+        });
     });
 
+    LW_TRACE("Loop starting.");
     started = true;
     loop.run();
     finished = true;
+    LW_TRACE("Loop completed.");
 
     EXPECT_TRUE(promise_called);
 }
@@ -79,7 +100,7 @@ TEST_F(PipeTests, StopRead){
     bool promise_called = false;
 
     // Set up the pipe to read.
-    pipe.open(pipes[0]);
+    pipe.open(pipe_descriptors[0]);
     pipe.read([&](const std::shared_ptr<const memory::Buffer>& buffer){
         EXPECT_TRUE(started);
         EXPECT_FALSE(finished);
@@ -95,8 +116,8 @@ TEST_F(PipeTests, StopRead){
 
     // Separately, create a timeout that will send the data on the pipe once the loop has started.
     event::wait(loop, 0s).then([&](){
-        ::write(pipes[1], content_str.c_str(), content_str.size());
-        ::fsync(pipes[1]);
+        ::write(pipe_descriptors[1], content_str.c_str(), content_str.size());
+        ::fsync(pipe_descriptors[1]);
         // No close here, should stop on its own.
     });
 
@@ -118,7 +139,7 @@ TEST_F(PipeTests, Write){
     std::shared_ptr<memory::Buffer> data(&contents, [](memory::Buffer*){});
 
     // Set up the pipe to write.
-    pipe.open(pipes[1]);
+    pipe.open(pipe_descriptors[1]);
     pipe.write(data).then([&](const std::size_t bytes_written){
         promise_called = true;
         EXPECT_TRUE(started);
@@ -129,7 +150,7 @@ TEST_F(PipeTests, Write){
     // Separately, create a timeout that read the data from the pipe once the loop has started.
     event::wait(loop, 0s).then([&](){
         memory::Buffer buffer(1024);
-        int bytes_read = ::read(pipes[0], buffer.data(), buffer.capacity());
+        int bytes_read = ::read(pipe_descriptors[0], buffer.data(), buffer.capacity());
 
         EXPECT_EQ((int)contents.size(), bytes_read);
         EXPECT_EQ(contents, memory::Buffer(buffer.data(), bytes_read));
@@ -141,6 +162,42 @@ TEST_F(PipeTests, Write){
 
     EXPECT_TRUE(promise_called);
 }
+
+// ---------------------------------------------------------------------------------------------- //
+
+// TEST_F(PipeTests, BindRead){
+//     io::Pipe pipe(loop);
+//     bool started = false;
+//     bool finished = false;
+//     bool promise_called = false;
+//
+//     // Set up the pipe to read.
+//     pipe.bind(pipe_name);
+//     pipe.read([&](const std::shared_ptr<const memory::Buffer>& buffer){
+//         EXPECT_TRUE(started);
+//         EXPECT_FALSE(finished);
+//         EXPECT_EQ(contents, *buffer);
+//     }).then([&](const std::size_t bytes_read){
+//         promise_called = true;
+//         EXPECT_TRUE(started);
+//         EXPECT_FALSE(finished);
+//         EXPECT_EQ(contents.size(), bytes_read);
+//     });
+//
+//     // Separately, create a timeout that will send the data on the pipe once the loop has started.
+//     event::wait(loop, 0s).then([&](){
+//         int pipe_fd = ::open(pipe_name.c_str(), O_WRONLY);
+//         ::write(pipe_fd, content_str.c_str(), content_str.size());
+//         ::fsync(pipe_fd);
+//         ::close(pipe_fd);
+//     });
+//
+//     started = true;
+//     loop.run();
+//     finished = true;
+//
+//     EXPECT_TRUE(promise_called);
+// }
 
 }
 }
